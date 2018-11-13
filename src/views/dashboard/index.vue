3291862391dad59491d279eda3b0d8e7961a33a9
YuKongEr <541130126@qq.com>
2018-11-13 18:54:08 +0800

    :art: 修改首页样式

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,32 +1,18 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name:{{name}}</div>
-    <div class="dashboard-text">roles:<span v-for='role in roles' :key='role'>{{role}}</span></div>
+    <component :is="currentRole" />
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-
+import adminDashboard from './admin'
 export default {
-  name: 'dashboard',
-  computed: {
-    ...mapGetters([
-      'name',
-      'roles'
-    ])
+  name: 'Dashboard',
+  components: { adminDashboard },
+  data() {
+    return {
+      currentRole: 'adminDashboard'
+    }
   }
 }
 </script>
-
-<style rel="stylesheet/scss" lang="scss" scoped>
-.dashboard {
-  &-container {
-    margin: 30px;
-  }
-  &-text {
-    font-size: 30px;
-    line-height: 46px;
-  }
-}
-</style>
